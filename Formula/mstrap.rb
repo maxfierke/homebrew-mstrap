d089b3450ae0910f9b1fc84df349fbda7a10bd75
Max Fierke <max@maxfierke.com>
2020-12-02 17:24:44 -0600

    mstrap 0.2.4 (new formula)

---
+++ Formula/mstrap.rb
@@ -0,0 +1,20 @@
+class Mstrap < Formula
+  desc "Machine Bootstrapping tool with a focus on sensible defaults, conventions, and avoidance of vendoring"
+  homepage "https://mstrap.dev"
+  url "https://github.com/maxfierke/mstrap/archive/v0.2.4.tar.gz"
+  sha256 "6ddebd447fd5d89cf25583d6535d3ad37e60c8974205055c3a29a848153a7854"
+  license "MIT"
+
+  depends_on "bash" => :build
+  depends_on "crystal" => :build
+  depends_on "make" => :build
+
+  def install
+    system "make", "build", "RELEASE=1"
+    bin.install "bin/mstrap"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
