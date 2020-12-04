b4928807bd212deac22427100b6f02c1cda88aca
Max Fierke <max@maxfierke.com>
2020-12-03 21:53:06 -0600

    Fix some brew audit warnings

--- Formula/mstrap.rb
+++ Formula/mstrap.rb
@@ -1,17 +1,17 @@
 class Mstrap < Formula
-  desc "Machine Bootstrapping tool with a focus on sensible defaults, conventions, and avoidance of vendoring"
+  desc "Machine bootstrapping tool with a focus on conventions"
   homepage "https://mstrap.dev"
   url "https://github.com/maxfierke/mstrap/archive/v0.2.4.tar.gz"
   sha256 "6ddebd447fd5d89cf25583d6535d3ad37e60c8974205055c3a29a848153a7854"
   license "MIT"
 
-  depends_on "openssl"
-  depends_on "pcre"
-  depends_on "libevent"
-
   depends_on "bash" => :build
   depends_on "crystal" => :build
   depends_on "make" => :build
+
+  depends_on "libevent"
+  depends_on "openssl"
+  depends_on "pcre"
 
   def install
     system "make", "build", "RELEASE=1"
@@ -19,6 +19,7 @@ class Mstrap < Formula
   end
 
   test do
-    assert_match "mstrap - mstrap is a tool for bootstrapping development machines", shell_output(bin/"mstrap --help", 0)
+    assert_match "mstrap is a tool for bootstrapping development machines",
+      shell_output(bin/"mstrap --help")
   end
 end
